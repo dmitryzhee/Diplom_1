a5c1ef468297afe2a93d02e59f41fcfb4943ebf0
Dmitry Zhegalov <dmitryzhee@gmail.com>
2023-09-25 16:42:07 +0300

    Updated BunTest

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,2 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import praktikum.Bun;
+
 public class BunTest {
+Bun bun;
+
+  @Test
+  public void getNameSuccess() {
+    bun = new Bun("Ржаная", 0);
+    Assert.assertEquals(bun.getName(), "Ржаная");
+  }
+
+  @Test
+  public void getPriceSuccess() {
+    bun = new Bun("Ржаная", 5.2f);
+    Assert.assertEquals(bun.getPrice(), 5.2f, 0);
+  }
 }
